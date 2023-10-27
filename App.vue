9e2ca9e0bf0d9f4b532469907bbbc658ab401203
Nerak1106 <142179750+Nerak1106@users.noreply.github.com>
2023-10-27 10:09:04 -0500

    Update App.vue

--- App.vue
+++ App.vue
@@ -1,15 +1,22 @@
 <template>
-<div class="container">
-  <nav class="navbar navbar-expand navbar-light bg-light">
+  <div class="container">
+    <nav class="navbar navbar-expand navbar-light bg-light">
       <div class="nav navbar-nav">
-          
-          <router-link to="/crear" class="navbar-brand">Crear</router-link> 
-          <router-link to="/listar" class="navbar-brand">Listar</router-link>      
+        <router-link to="/crear" class="navbar-brand">Crear</router-link>
+        <router-link to="/listar" class="navbar-brand">Listar</router-link>
       </div>
-  </nav>
-</div>
-<br>
+    </nav>
+  </div>
+  <br>
   <router-view/>
 </template>
+
+<style scoped>
+.navbar-brand:hover {
+  color: #40B390; 
+  transition: color 0.3s; 
+}
+
+</style>
 
 
